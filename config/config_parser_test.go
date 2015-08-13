5627d8de31dbb4955fdb890a7e546d04b6bfa08e
Jeff Pierce <jpierce@change.org>
2015-08-12 21:23:31 -0700

    Fixed test.

--- config/config_parser_test.go
+++ config/config_parser_test.go
@@ -1,21 +1,19 @@
 package config
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
 
-// Check to make sure config parser properly parses test field.  All fields are defined in test yaml.
 func TestParseConfig(t *testing.T) {
 	// Load yaml into config struct
 	config := ParseConfig("config_test.yaml")
 
-	// iterate over struct values to ensure all fields loaded.
-	s := reflect.ValueOf(&config).Elem()
-	for i := 0; i < s.NumField(); i++ {
-		f := s.Field(i)
-		if f.Interface() == nil {
-			t.Errorf("Config file did not parse properly, found empty field.")
-		}
+	// Ensure we have a config.CassabonConfig
+	typevar := reflect.TypeOf(config).Kind()
+	fmt.Println("Our config is a struct:", typevar == reflect.Struct)
+	if typevar != reflect.Struct {
+		t.Errorf("We received a %v back from the config parser, expected struct.", typevar)
 	}
 }
