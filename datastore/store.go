294e7d2fb6bb5e036382b4328b5157163536328a
Marcus Redivo <marcus@change.org>
2015-09-14 14:51:06 -0700

    Add representation to permit walking paths by expression matched.

--- datastore/store.go
+++ datastore/store.go
@@ -6,11 +6,17 @@ import (
 	"github.com/jeffpierce/cassabon/config"
 )
 
-// rollups contains the accumulated metrics data for a path.
-type rollups struct {
+// rollup contains the accumulated metrics data for a path.
+type rollup struct {
 	expr  string    // The text form of the path expression, to locate the definition
 	count []uint64  // The number of data points accumulated (for averaging)
 	value []float64 // One rollup per window definition
+}
+
+// runlist contains the paths to be written for an expression, and when to write the rollups.
+type runlist struct {
+	nextWrite []time.Time        // The next write time for each rollup bucket
+	path      map[string]*rollup // The rollup data for each path matched by the expression
 }
 
 type StoreManager struct {
@@ -20,8 +26,9 @@ type StoreManager struct {
 	timeout    chan struct{}      // Timeout notifications arrive on this channel
 
 	// Rollup data.
-	metricData map[string]rollups // Stats, by path, broken out by rollup window duration.
-	maxTimeout time.Duration      // The duration of the shortest rollup window.
+	byPath     map[string]*rollup  // Stats, by path, for rollup accumulation
+	byExpr     map[string]*runlist // Stats, by path within expression, for rollup processing
+	maxTimeout time.Duration       // The duration of the shortest rollup window.
 }
 
 func (sm *StoreManager) Init() {
@@ -31,7 +38,15 @@ func (sm *StoreManager) Init() {
 	sm.timeout = make(chan struct{}, 1)
 
 	// Initialize rollup data structures.
-	sm.metricData = make(map[string]rollups)
+	sm.byPath = make(map[string]*rollup)
+	sm.byExpr = make(map[string]*runlist)
+	for expr, rollupdef := range config.G.Rollup {
+		// For each expression, provide a place to record all the paths that it matches.
+		rl := new(runlist)
+		rl.nextWrite = make([]time.Time, len(rollupdef.Windows))
+		rl.path = make(map[string]*rollup)
+		sm.byExpr[expr] = rl
+	}
 	sm.maxTimeout = 5
 
 	// Start the persistent goroutines.
@@ -127,56 +142,59 @@ func (sm *StoreManager) accumulate(metric config.CarbonMetric) {
 	config.G.Log.System.LogDebug("StoreManager::accumulate")
 
 	// Locate the metric in the map.
-	var currentRollups rollups
+	var currentRollup *rollup
 	var found bool
-	if currentRollups, found = sm.metricData[metric.Path]; !found {
+	if currentRollup, found = sm.byPath[metric.Path]; !found {
 
 		// Determine which expression matches this path.
-		for _, expr := range config.G.RollupPriority {
+		var expr string
+		for _, expr = range config.G.RollupPriority {
 			if expr != config.CATCHALL_EXPRESSION {
 				if config.G.Rollup[expr].Expression.MatchString(metric.Path) {
-					currentRollups.expr = expr
 					break
 				}
-			} else {
-				currentRollups.expr = config.CATCHALL_EXPRESSION
 			}
+			// Catchall always appears last, and is therefore the default value.
 		}
 
-		currentRollups.count = make([]uint64, len(config.G.Rollup[currentRollups.expr].Windows))
-		currentRollups.value = make([]float64, len(config.G.Rollup[currentRollups.expr].Windows))
+		// Initialize, and insert the new rollup into both maps.
+		currentRollup = new(rollup)
+		currentRollup.expr = expr
+		currentRollup.count = make([]uint64, len(config.G.Rollup[expr].Windows))
+		currentRollup.value = make([]float64, len(config.G.Rollup[expr].Windows))
+		sm.byPath[metric.Path] = currentRollup
+		sm.byExpr[expr].path[metric.Path] = currentRollup
 	}
 
 	// Apply the incoming metric to each rollup bucket.
-	switch config.G.Rollup[currentRollups.expr].Method {
+	switch config.G.Rollup[currentRollup.expr].Method {
 	case config.AVERAGE:
-		for i, v := range currentRollups.value {
-			currentRollups.value[i] = (v*float64(currentRollups.count[i]) + metric.Value) /
-				float64(currentRollups.count[i]+1)
+		for i, v := range currentRollup.value {
+			currentRollup.value[i] = (v*float64(currentRollup.count[i]) + metric.Value) /
+				float64(currentRollup.count[i]+1)
 		}
 	case config.MAX:
-		for i, v := range currentRollups.value {
+		for i, v := range currentRollup.value {
 			if v < metric.Value {
-				currentRollups.value[i] = metric.Value
+				currentRollup.value[i] = metric.Value
 			}
 		}
 	case config.MIN:
-		for i, v := range currentRollups.value {
+		for i, v := range currentRollup.value {
 			if v > metric.Value {
-				currentRollups.value[i] = metric.Value
+				currentRollup.value[i] = metric.Value
 			}
 		}
 	case config.SUM:
-		for i, v := range currentRollups.value {
-			currentRollups.value[i] = v + metric.Value
+		for i, v := range currentRollup.value {
+			currentRollup.value[i] = v + metric.Value
 		}
 	}
-	for i, _ := range currentRollups.count {
-		currentRollups.count[i]++
-	}
 
-	// Save the updated structure.
-	sm.metricData[metric.Path] = currentRollups
+	// Note that we added a data point into each bucket.
+	for i, _ := range currentRollup.count {
+		currentRollup.count[i]++
+	}
 }
 
 // flush persists the accumulated metrics to the database.
