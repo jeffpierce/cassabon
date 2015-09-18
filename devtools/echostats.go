65c3634f7c0b41d4c67c59065e1c715616dcf8e6
Marcus Redivo <marcus@change.org>
2015-09-17 17:10:56 -0700

    Provide a program to dump statsd data to stdout

---
+++ devtools/echostats.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	addr := net.UDPAddr{Port: 8125, IP: net.ParseIP("127.0.0.1")}
+	conn, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		if n, _, err := conn.ReadFromUDP(buf); err == nil {
+			fmt.Println(string(buf[0:n]))
+		} else {
+			fmt.Println("Error: ", err)
+		}
+	}
+}
