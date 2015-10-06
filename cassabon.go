fe228521b14a14823a8fbd0656dac2305225a239
Marcus Redivo <marcus@change.org>
2015-10-06 14:28:12 -0700

    Remove redundant element from log filenames

--- cassabon.go
+++ cassabon.go
@@ -42,9 +42,9 @@ func main() {
 	sev, errLogLevel := logging.TextToSeverity(config.G.Log.Loglevel)
 	if config.G.Log.Logdir != "" {
 		logDir, _ := filepath.Abs(config.G.Log.Logdir)
-		config.G.Log.System.Open(filepath.Join(logDir, "cassabon.system.log"), sev)
-		config.G.Log.Carbon.Open(filepath.Join(logDir, "cassabon.carbon.log"), logging.Unclassified)
-		config.G.Log.API.Open(filepath.Join(logDir, "cassabon.api.log"), logging.Unclassified)
+		config.G.Log.System.Open(filepath.Join(logDir, "system.log"), sev)
+		config.G.Log.Carbon.Open(filepath.Join(logDir, "carbon.log"), logging.Unclassified)
+		config.G.Log.API.Open(filepath.Join(logDir, "api.log"), logging.Unclassified)
 	} else {
 		config.G.Log.System.Open("", sev)
 		config.G.Log.Carbon.Open("", logging.Unclassified)
