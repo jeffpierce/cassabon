3c5bb1216b9219628c799e512774dfbcc76efac1
Marcus Redivo <marcus@change.org>
2015-10-14 12:49:12 -0700

    Use batched statements when performing inserts to Cassandra

---
+++ datastore/batchwriter.go
@@ -0,0 +1,59 @@
+package datastore
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gocql/gocql"
+)
+
+type batchWriter struct {
+	dbClient  *gocql.Session
+	keyspace  string
+	batchSize int
+
+	batch     *gocql.Batch
+	stmtCount int
+	stmt      string
+}
+
+// Init
+func (bw *batchWriter) Init(dbClient *gocql.Session, keyspace string, batchSize int) {
+	bw.dbClient = dbClient
+	bw.keyspace = keyspace
+	bw.batchSize = batchSize
+}
+
+// Size
+func (bw *batchWriter) Size() int {
+	return bw.stmtCount
+}
+
+// Prepare
+func (bw *batchWriter) Prepare(table string) {
+	bw.batch = gocql.NewBatch(gocql.LoggedBatch)
+	bw.stmtCount = 0
+	bw.stmt = fmt.Sprintf(
+		`INSERT INTO %s.%s (path, time, stat) VALUES (?, ?, ?)`, bw.keyspace, table)
+}
+
+// Append
+func (bw *batchWriter) Append(path string, ts time.Time, value float64) error {
+	bw.batch.Query(bw.stmt, path, ts, value)
+	bw.stmtCount++
+	if bw.stmtCount >= bw.batchSize {
+		return bw.Write()
+	} else {
+		return nil
+	}
+}
+
+// Write
+func (bw *batchWriter) Write() error {
+	if bw.stmtCount > 0 {
+		bw.stmtCount = 0
+		return bw.dbClient.ExecuteBatch(bw.batch)
+	} else {
+		return nil
+	}
+}
