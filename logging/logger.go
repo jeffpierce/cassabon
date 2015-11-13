5900139eb07fda92c34e2b06286a25c698cb9602
Marcus Redivo <marcus@change.org>
2015-11-12 17:39:47 -0800

    Eliminate logging race in SetLogLevel()

--- logging/logger.go
+++ logging/logger.go
@@ -121,8 +121,10 @@ func (l *FileLogger) init(logFacility string) {
 
 // SetLogLevel updates the logging level threshold with imediate effect.
 func (l *FileLogger) SetLogLevel(logLevel Severity) {
+	l.m.Lock()
+	defer l.m.Unlock()
 	l.logLevel = logLevel
-	l.LogInfo("Log level set to %s", SeverityToText(logLevel))
+	l.emit(Info, "Log level set to %s", SeverityToText(logLevel))
 }
 
 // GetLogLevel returns the current logging level threshold.
